8894ac7fddcaa5c2ee757988be63a4d70632473b
arthuraleixovital <arthuraleixovital@outlook.com.br>
2025-01-22 16:13:19 -0300

    Criação da pagina Questões.

--- src/routes/questões/+page.svelte
+++ src/routes/questões/+page.svelte
@@ -4,12 +4,31 @@
 	let { form } = $props();
 </script>
 
-<h1>Nova categoria</h1>
+<h1>Nova questão</h1>
 <form method="post" action="?/criar" use:enhance>
 	<label>
-		Nome da categoria:
-		<input name="nome" />
+		Enunciado da questão:
+		<input name="enunciado" />
 	</label>
-	<button>Criar</button>
+	<br>
+	<label>
+		Alternativas:
+		<br>
+		1 <input name="alternativa1" />
+		<br>
+		2 <input name="alternativa2" />
+		<br>
+		3 <input name="alternativa3" />
+		<br>
+		4 <input name="alternativa4" />
+		<br>
+		5 <input name="alternativa5" />
+	</label>
+	<br>
+	<label>
+		Resposta:
+		<input name="resposta" type="number" min="1" max="5" step="1" />
+	</label>
+	<button>Criar questão.</button>
 </form>
 <p style="color: red">{form?.message ?? ''}</p>
