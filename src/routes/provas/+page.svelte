9729243ebe70aa6a46756fd70574e9e03ea04992
arthuraleixovital <arthuraleixovital@outlook.com.br>
2025-04-15 17:58:04 -0400

    Gabarito na prova.

--- src/routes/provas/+page.svelte
+++ src/routes/provas/+page.svelte
@@ -8,8 +8,21 @@
 {#if form}
 	<h2><b>{form.qtdprovas}</b> Provas Geradas:</h2>
 	{#each form.provas as prova, i}
-		<div class="prova">
+		<div class="prova"  style="page-break-before: always;">
 			<h3>Prova {i + 1}</h3>
+			<h4>Gabarito</h4>
+			<table>
+				{#each prova as questao, i}
+				<tr>
+					<td>{i + 1}</td>
+				<td>A</td>
+				<td>B</td>
+				<td>C</td>
+				<td>D</td>
+				<td>E</td>
+				</tr>
+				{/each}
+			</table>
 			{#each prova as questao, i}
 				<p>Questao {i + 1}</p>
 				<p><strong>Enunciado:</strong> {questao.enunciado}</p>
@@ -27,3 +40,10 @@
 {#if form.qtdprovas === 0}
 	<p>Nenhuma prova gerada ainda.</p>
 {/if}
+
+<style>
+	th, td {
+  border: 1px solid black;
+  border-radius: 10px;
+}
+</style>
